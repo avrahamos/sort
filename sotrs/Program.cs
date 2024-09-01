ee4a01ce4ca2cf2d925904fe5990b3d9770c7236
avraham <2721avi@gmail.com>
2024-09-01 10:41:57 +0300

    bubble sort

---
+++ sotrs/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace sotrs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 5, 7, 8, 3, 1, 0, 86, 4 };
+            Console.WriteLine("Original array:");
+            Console.WriteLine(string.Join(", ", array));
+
+            BubbleSort(array);
+
+            Console.WriteLine("Sorted array:");
+            Console.WriteLine(string.Join(", ", array));
+
+        }
+        static void BubbleSort(int[] array)
+        {
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                    break;
+            }
+        }
+    }
+}
